2a9c187f9e8fed5670e807aadd2aff486e6132d6
harris-miller <harrismillerconsulting@gmail.com>
2024-09-03 02:09:17 -0600

    i'm 99.9% sure that's right, lol

--- haskell/2016/Day9.hs
+++ haskell/2016/Day9.hs
@@ -13,23 +13,28 @@ parseMarker xs = ((s, r), drop 1 rest)
     (s, r) = tmap read $ second (drop 1) $ splitAt i2 marker
 
 extend :: String -> ((Int, Int), String) -> (String, String)
-extend acc ((sub, rep), s) = (concat (acc : replicate rep (take sub s)), drop sub s)
+extend acc ((sub, rep), s) = (reverse $ concat (replicate rep (take sub s)) ++ acc, drop sub s)
 
 process1 :: String -> String
-process1 = go []
+process1 = reverse . go []
   where
     go acc [] = acc
     go acc ('(' : xs) = uncurry go $ extend acc $ parseMarker xs
+    go acc (x : xs) = go (x : acc) xs
+
+extend2 :: ((Int, Int), String) -> String
+extend2 ((sub, rep), s) = concat (replicate rep (take sub s)) ++ drop sub s
+
+process2 :: String -> String
+process2 = reverse . go []
+  where
+    go acc [] = acc
+    go acc ('(' : xs) = go acc $ extend2 (parseMarker xs)
     go acc (x : xs) = go (acc <> [x]) xs
 
 main' :: IO ()
 main' = do
   contents <- head . lines <$> readFile "../inputs/2016/Day9/input.txt"
-  -- print $ length $ process1 "ADVENT"
-  -- print $ length $ process1 "A(1x5)BC"
-  -- print $ length $ process1 "(3x3)XYZ"
-  -- print $ length $ process1 "A(2x2)BCD(2x2)EFG"
-  -- print $ length $ process1 "(6x1)(1x3)A"
-  -- print $ length $ process1 "X(8x2)(3x3)ABCY"
   print $ length $ process1 contents
+  print $ length $ process2 contents
   return ()

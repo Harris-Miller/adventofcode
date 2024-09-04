78a26e2a652075c11c07740ee4f7dd1dfc651917
harris-miller <harrismillerconsulting@gmail.com>
2024-09-04 00:00:00 -0600

    ts 2016 day13

--- typescript/2016/day13.ts
+++ typescript/2016/day13.ts
@@ -1,5 +1,5 @@
-import { breadthFirstSearch } from 'fp-search-algorithms';
-import { equals } from 'ramda';
+import { breadthFirstSearch, breadthFirstTraversal } from 'fp-search-algorithms';
+import * as R from 'ramda';
 
 type Space = 'Open' | 'Wall';
 
@@ -14,16 +14,29 @@ const calcSpace = ([x, y]: [number, number]): Space => {
 const start: [number, number] = [1, 1];
 const end: [number, number] = [31, 39];
 
-const get4Neighbors = ([x, y]: [number, number]): [number, number][] => [
-  [x - 1, y],
-  [x, y + 1],
-  [x + 1, y],
-  [x, y - 1],
-];
+const get4Neighbors = ([x, y]: [number, number]): [number, number][] => {
+  const a: [number, number][] = [
+    [x - 1, y],
+    [x, y + 1],
+    [x + 1, y],
+    [x, y - 1],
+  ];
+
+  return a.filter(([l, r]) => l >= 0 && r >= 0);
+};
 
 const getNextState = (state: [number, number]): [number, number][] =>
   get4Neighbors(state).filter(n => calcSpace(n) === 'Open');
 
-const r1 = breadthFirstSearch(getNextState, equals(end), start);
+const r1 = breadthFirstSearch(getNextState, R.equals(end), start);
 
-console.log(r1[1].length - 1);
+console.log(r1![1].length - 1);
+
+const visited = new Set<string>();
+for (const [value, pathTo] of breadthFirstTraversal(getNextState, start)) {
+  visited.add(R.toString(value));
+  if (pathTo.length > 50) break;
+}
+
+console.log(visited);
+console.log(visited.size);

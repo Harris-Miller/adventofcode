965a19666cdf4d5523c3e03b005c7588c9b52551
harris-miller <harrismillerconsulting@gmail.com>
2024-10-20 13:18:48 -0600

    Haskell 2018 Day7

--- haskell/2018/Day7.hs
+++ haskell/2018/Day7.hs
@@ -56,42 +56,43 @@ data SS = SS {graph :: HashSet Edge, workers :: HashMap Int (Maybe (Char, Int))}
   deriving (Eq, Show)
 
 nodeTime :: Char -> Int
-nodeTime c = fromEnum c - 64
+-- nodeTime c = fromEnum c - 64 -- for sample
+nodeTime c = fromEnum c - 4 -- for input
 
 fakeAsyncTopSort :: HashSet Char -> RWST RR WW SS IO ()
 fakeAsyncTopSort todos = do
   SS {..} <- get
+  -- liftIO $ print todos
   let availWorkers = (M.keys . M.filter isNothing) workers
-  let nodesToStart = todos `S.difference` S.map fst graph
-  let remaining = todos `S.difference` nodesToStart
-  -- let (nodesToStart, remaining) = splitAt (length availWorkers) todos
+  let nodesToStart = S.filter (`notElem` S.map snd graph) todos
+
   let newWorkers = M.union (M.fromList $ zip availWorkers $ map (\n -> Just (n, nodeTime n)) (S.toList nodesToStart)) workers
   -- liftIO $ print (M.toList newWorkers)
+  let remaining = todos `S.difference` (S.fromList . map fst . catMaybes . M.elems) newWorkers
+  -- liftIO $ print remaining
   let timeToNextComplete = maybe 0 (minimum . map snd) $ ((\xs -> if null xs then Nothing else Just xs) <=< sequence . filter isJust . M.elems) newWorkers
   -- liftIO $ print timeToNextComplete
   tell (Sum timeToNextComplete)
 
-  let newWorkers' = M.map (fmap (second (timeToNextComplete -))) newWorkers
+  let newWorkers' = M.map (fmap (second (\x -> x - timeToNextComplete))) newWorkers
   let finishedWork = map fst . filter ((== 0) . snd) $ catMaybes $ M.elems newWorkers'
+  -- liftIO $ print finishedWork
   let newWorkers'' = M.map (>>= \v -> if snd v == 0 then Nothing else Just v) newWorkers'
-  -- liftIO $ mapM_ print (M.toList newWorkers')
+  -- liftIO $ print (M.toList newWorkers')
 
   nodeToNodes <- ask
-  let toRemove = concatMap (\n -> [(n, to) | to <- nodeToNodes ! n]) finishedWork
-  let nextGraph = S.difference graph $ S.fromList toRemove
+  let nextGraph = foldr (\n acc -> S.filter ((/= n) . fst) acc) graph finishedWork
 
   let nextState = SS {graph = nextGraph, workers = newWorkers''}
   put nextState
   -- liftIO $ print remaining
-  liftIO $ print nextState
-
+  -- liftIO $ print nextState
+  -- liftIO $ getLine >>= print
   if null remaining && all isNothing (M.elems newWorkers'') then return () else fakeAsyncTopSort remaining
-
--- return ()
 
 main' :: IO ()
 main' = do
-  edges <- map parse . lines <$> readFile "../inputs/2018/Day7/sample.txt"
+  edges <- map parse . lines <$> readFile "../inputs/2018/Day7/input.txt"
   let edgesSet = S.fromList edges
   let nodeToNodes = toMap edges
   let starts = sort $ collectStarts nodeToNodes
@@ -102,7 +103,7 @@ main' = do
 
   -- part 2
   let allNodes = M.keysSet nodeToNodes
-  (_, _, w) <- runRWST (fakeAsyncTopSort allNodes) nodeToNodes $ SS {graph = edgesSet, workers = M.fromList [(x, Nothing) | x <- [1 .. 2]]}
+  (_, _, w) <- runRWST (fakeAsyncTopSort allNodes) nodeToNodes $ SS {graph = edgesSet, workers = M.fromList [(x, Nothing) | x <- [1 .. 5]]}
   print w
 
   return ()

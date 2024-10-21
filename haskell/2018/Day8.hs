c3621fc26554f66bec3cc5a62b2da50b0a999158
harris-miller <harrismillerconsulting@gmail.com>
2024-10-21 00:11:28 -0600

    Haskell 2018 Day8 part1

--- haskell/2018/Day8.hs
+++ haskell/2018/Day8.hs
@@ -1,8 +1,32 @@
 module Day8 where
 
-import Data.Graph
+import Control.Monad.Writer
+
+readNodeInfo :: [Int] -> ([Int], (Int, Int))
+readNodeInfo (c : m : xs) = (xs, (c, m))
+
+processChildren :: Int -> [Int] -> Writer (Sum Int) [Int]
+processChildren 0 xs = return xs
+processChildren x xs = do
+  xs' <- processNode xs
+  processChildren (x - 1) xs'
+
+processNode :: [Int] -> Writer (Sum Int) [Int]
+processNode xs = do
+  let (x : m : rest) = xs
+  rest' <- processChildren x rest
+  let (ms, rest'') = splitAt m rest'
+  tell $ mconcat (map Sum ms)
+  return rest''
+
+processTop :: [Int] -> Writer (Sum Int) ()
+processTop [] = return ()
+processTop xs = do
+  xs' <- processNode xs
+  processTop xs'
 
 main' :: IO ()
 main' = do
   contents <- map read . words . head . lines <$> readFile "../inputs/2018/Day8/input.txt" :: IO [Int]
-  mapM_ print contents
+  let r = execWriter $ processTop contents
+  print r

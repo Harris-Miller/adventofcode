045ed9eef31a2316dabe37f0e7c2b39e0a7f5bac
harris-miller <harrismillerconsulting@gmail.com>
2024-12-02 18:51:12 -0700

    improving 2024 day1 Haskell a bit

--- haskell/2024/Day1.hs
+++ haskell/2024/Day1.hs
@@ -5,23 +5,22 @@ import Data.Map (Map)
 import Data.Map qualified as M
 import Data.Maybe
 
-parse :: String -> [Int]
+parse :: String -> (Int, Int)
 parse s =
   let [(a, s2)] = (reads :: ReadS Int) s
       [(b, _)] = (reads :: ReadS Int) s2
-   in [a, b]
+   in (a, b)
 
 main' :: IO ()
 main' = do
   contents <- lines <$> readFile "../inputs/2024/Day1/input.txt"
   let pairs = map parse contents
 
-  let asLists = transpose pairs
-  let sorted = (transpose . map sort) asLists
-  let r1 = sum $ map (abs . foldr1 (-)) sorted
+  let (ll, rr) = unzip pairs
+  let sorted = zip (sort ll) (sort rr)
+  let r1 = sum $ map (abs . uncurry (-)) sorted
   print r1
 
-  let [ll, rr] = asLists
-  let rMap = foldr (\x -> M.insertWith (+) x 1) M.empty rr
-  let r2 = sum $ mapMaybe (\x -> (* x) <$> M.lookup x rMap) ll
+  let rCounts = foldr (\x -> M.insertWith (+) x 1) M.empty rr
+  let r2 = sum $ mapMaybe (\x -> (* x) <$> M.lookup x rCounts) ll
   print r2

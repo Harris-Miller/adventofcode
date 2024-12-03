6fec843af4eae12f88135802c24ea2325d6b01d7
harris-miller <harrismillerconsulting@gmail.com>
2024-12-02 23:50:21 -0700

    2024 day3 part2 - typescript

--- typescript/2024/day3.ts
+++ typescript/2024/day3.ts
@@ -6,16 +6,35 @@ const content = (await Bun.file('../inputs/2024/Day3/input.txt').text()).trim().
 
 const rMul = /mul\(\d{1,4},\d{1,4}\)/g;
 
-const muls = content.match(rMul)!;
+const parsed = content.match(rMul)!;
 
-const r1 = muls.map(
-  R.pipe(
-    (s: string) => R.drop(4, s),
-    s => R.init(s),
-    R.split(','),
-    R.map(parseInt10),
-    R.product,
-  ),
+const doMul = R.pipe(
+  (s: string) => R.drop(4, s),
+  s => R.init(s),
+  R.split(','),
+  R.map(parseInt10),
+  R.product,
 );
 
-console.log(R.sum(r1));
+const r1 = R.sum(parsed.map(doMul));
+
+console.log(r1);
+
+const rMul2 = /(mul\(\d{1,4},\d{1,4}\)|do\(\)|don't\(\))/g;
+
+const parsed2 = content.match(rMul2)!;
+
+let isOn = true;
+let r2 = 0;
+
+for (const m of parsed2) {
+  if (m.startsWith("don't")) {
+    isOn = false;
+  } else if (m.startsWith('do')) {
+    isOn = true;
+  } else if (isOn) {
+    r2 += doMul(m);
+  }
+}
+
+console.log(r2);

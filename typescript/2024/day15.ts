eb680281e7fa2700d0ce56df81536b0650dd9cb9
harris-miller <harrismillerconsulting@gmail.com>
2024-12-16 20:24:47 -0700

    2024 day15 part1 typescript

---
+++ typescript/2024/day15.ts
@@ -0,0 +1,99 @@
+import * as R from 'ramda';
+
+import type { Coord, Grid } from '../lib/grid';
+import { gridAsIsToString, parseGridAsIs, stringToCoord } from '../lib/grid';
+
+const content = (await Bun.file('../inputs/2024/Day15/input.txt').text()).trim();
+
+type Direction = '^' | '<' | '>' | 'v';
+
+const parse = () => {
+  const temp = content.split('\n\n');
+  return [parseGridAsIs(temp[0]), temp[1].split('').filter(c => c !== '\n') as Direction[]] as const;
+};
+
+const [[, warehouse], movements] = parse();
+
+console.log(gridAsIsToString(warehouse));
+
+const start = stringToCoord(warehouse.entries().find(([, val]) => val === '@')![0]);
+
+const getNextPos = (dir: Direction, [r, c]: Coord): Coord => {
+  switch (dir) {
+    case '^':
+      return [r - 1, c];
+    case '>':
+      return [r, c + 1];
+    case 'v':
+      return [r + 1, c];
+    case '<':
+      return [r, c - 1];
+    default:
+      throw new Error('non-exhaustive');
+  }
+};
+
+const findFirstEmpty = (grid: Grid, dir: Direction, boxPos: Coord): Coord[] | undefined => {
+  let pos = boxPos;
+  let val = grid.get(R.toString(pos));
+  const coords: Coord[] = [];
+  while (R.isNotNil(val)) {
+    if (val === '#') return undefined;
+    coords.push(pos);
+    if (val === '.') return coords;
+    pos = getNextPos(dir, pos);
+    val = grid.get(R.toString(pos));
+  }
+  return undefined;
+};
+
+const pushBoxes = (grid: Grid, coords: Coord[]): Grid => {
+  const [first, ...rest] = coords;
+  rest.forEach(c => grid.set(R.toString(c), 'O'));
+  grid.set(R.toString(first), '.');
+  return grid;
+};
+
+const tryMove = (grid: Grid, dir: Direction, current: Coord): [Grid, Coord] => {
+  const nextPos = getNextPos(dir, current);
+  const valAtNextPos = warehouse.get(R.toString(nextPos))!;
+
+  switch (valAtNextPos) {
+    case '#':
+      return [grid, current];
+    // @ts-expect-error
+    case 'O': {
+      const firstEmptySpace = findFirstEmpty(grid, dir, nextPos);
+      if (R.isNil(firstEmptySpace)) return [grid, current];
+      // eslint-disable-next-line no-param-reassign
+      grid = pushBoxes(grid, firstEmptySpace);
+      // let fall-through
+    }
+    // eslint-disable-next-line no-fallthrough
+    default:
+      grid.set(R.toString(current), '.').set(R.toString(nextPos), '@');
+      return [grid, nextPos];
+  }
+};
+
+const [endGrid] = movements.reduce<[Grid, Coord]>(
+  ([grid, current], dir) => {
+    const next = tryMove(grid, dir, current);
+    // console.log(gridAsIsToString(next[0]));
+    return next;
+  },
+  [warehouse, start],
+);
+
+const r1 = R.sum(
+  endGrid
+    .entries()
+    .filter(([, val]) => val === 'O')
+    .map(([cStr]) => {
+      const [r, c] = stringToCoord(cStr);
+      return r * 100 + c;
+    })
+    .toArray(),
+);
+
+console.log(r1);

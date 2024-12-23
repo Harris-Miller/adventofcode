a4de21e2b8b601545278b7b357d9cce195b8747d
harris-miller <harrismillerconsulting@gmail.com>
2024-12-23 01:11:55 -0700

    final grid fix for 2024

--- typescript/2024/day14.ts
+++ typescript/2024/day14.ts
@@ -1,18 +1,15 @@
-// import { depthFirstSearch } from 'fp-search-algorithms';
+import { Dict } from 'fp-search-algorithms';
 import * as R from 'ramda';
 
 import { parseInt10 } from '../lib/fp';
-import { gridToString } from '../lib/grid';
-import type { Coord } from '../lib/grid';
-
-// import { parseInt10 } from '../lib/fp';
-// import type { Coord } from '../lib/grid';
+import type { Point } from '../lib/gridRaw';
+import { dictToString } from '../lib/gridRaw';
 
 const content = (await Bun.file('../inputs/2024/Day14/input.txt').text()).trim();
 
 // console.log(content);
 
-type Robot = [p: Coord, v: Coord];
+type Robot = [p: Point, v: Point];
 
 const parse = (line: string) => line.split(' ').map(s => R.drop(2, s).split(',').reverse().map(parseInt10)) as Robot;
 
@@ -20,26 +17,20 @@ const robots = content.split('\n').map(parse);
 
 // console.log(robots);
 
-// const rMax = 7;
-// const cMax = 11;
-const rMax = 103;
-const cMax = 101;
+// const rLen = 7;
+// const cLen = 11;
+const rLen = 103;
+const cLen = 101;
 
-const rUpTo = (rMax - 1) / 2;
-const cUpTo = (cMax - 1) / 2;
+const rUpTo = (rLen - 1) / 2;
+const cUpTo = (cLen - 1) / 2;
 
 const robotToGrid = (rs: Robot[]) =>
-  new Map(
-    rs
-      .map(([p]) => R.toString(p))
-      .reduce((acc, p) => {
-        const val = acc.get(p);
-        acc.set(p, (val ?? 0) + 1);
-        return acc;
-      }, new Map<string, number>())
-      .entries()
-      .map<[string, string]>(([k, v]) => [k, v.toString()]),
-  );
+  rs.reduce((acc, [p]) => {
+    const val = acc.get(p);
+    acc.set(p, (val ?? 0) + 1);
+    return acc;
+  }, new Dict<Point, number>());
 
 const sample: Robot[] = [
   [
@@ -54,9 +45,9 @@ const sample: Robot[] = [
 // console.log('');
 
 const move = ([p, v]: Robot): Robot => {
-  const r = (p[0] + v[0]) % rMax;
-  const c = (p[1] + v[1]) % cMax;
-  return [[r < 0 ? rMax + r : r, c < 0 ? cMax + c : c], v];
+  const r = (p[0] + v[0]) % rLen;
+  const c = (p[1] + v[1]) % cLen;
+  return [[r < 0 ? rLen + r : r, c < 0 ? cLen + c : c], v];
 };
 
 const moved = R.range(0, 100)
@@ -70,8 +61,12 @@ const moved = R.range(0, 100)
 
 // console.log(moved);
 
-const grid2 = robotToGrid(moved);
-const asString2 = gridToString(rMax - 1, cMax - 1, '.', grid2);
+const grid2 = new Dict(
+  robotToGrid(moved)
+    .entries()
+    .map<[Point, string]>(([k, v]) => [k, v.toString()]),
+);
+const asString2 = dictToString(rLen - 1, cLen - 1, '.', grid2);
 console.log(asString2);
 
 const quads = R.collectBy((robot: Robot) => {

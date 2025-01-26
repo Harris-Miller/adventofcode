4615f3e30440bde1a3f577d0d5f3515c71d010ef
harris-miller <harrismillerconsulting@gmail.com>
2025-01-25 22:37:25 -0700

    WIP

--- typescript/2024/day22.ts
+++ typescript/2024/day22.ts
@@ -1,3 +1,7 @@
+import * as R from 'ramda';
+
+import { parseInt10 } from '../lib/fp';
+
 const content = (await Bun.file('../inputs/2024/Day22/input.txt').text()).trim().split('\n').map(BigInt);
 
 // console.log(content);
@@ -14,6 +18,46 @@ const doTheThing = (secret: bigint): bigint => {
 
 const sumBigInt = (arr: bigint[]) => arr.reduce((a, b) => a + b, BigInt(0));
 
-const part1 = sumBigInt(content.map(v => Array(2000).fill(undefined).reduce<bigint>(doTheThing, v)));
+const scanned = content.map(v => R.scan(doTheThing, v, Array(2000).fill(undefined)));
+const lasts = scanned.map(a => R.last(a)!);
+
+const part1 = sumBigInt(lasts);
 
 console.log(part1);
+
+const firstDigits = scanned.map(ss => ss.map(s => parseInt10(R.last(s.toString())!)));
+
+console.log(firstDigits[0]);
+
+const doThing2 = (seq: number[]) => {
+  const rtn = new Map<string, number>();
+  for (let i = 4; i < seq.length; i++) {
+    rtn.set(
+      [seq[i - 3] - seq[i - 4], seq[i - 2] - seq[i - 3], seq[i - 1] - seq[i - 2], seq[i] - seq[i - 1]].toString(),
+      seq[i],
+    );
+  }
+  return rtn;
+};
+
+const mapped = firstDigits.map(doThing2);
+
+const seen = new Set<string>();
+let bestSeq: string;
+let bestVal = 0;
+
+for (const map of mapped) {
+  // eslint-disable-next-line @typescript-eslint/no-loop-func
+  map.forEach((_, k) => {
+    if (seen.has(k)) return;
+    seen.add(k);
+    const r = R.sum(mapped.map(m => m.get(k) ?? 0));
+    if (r > bestVal) {
+      bestVal = r;
+      bestSeq = k;
+    }
+  });
+}
+
+// @ts-expect-error
+console.log(bestSeq, bestVal);

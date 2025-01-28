c79ea24b3c80454abd9992cb730e59ad887e19f2
harris-miller <harrismillerconsulting@gmail.com>
2025-01-28 00:13:40 -0700

    meh

--- haskell/2024/Day3.hs
+++ haskell/2024/Day3.hs
@@ -1,34 +1,65 @@
 module Day3 where
 
+import Control.Monad.State.Strict as St
 import Data.Either
-import Data.List
 import Text.Parsec.Char
 import Text.ParserCombinators.Parsec
 
--- mul :: GenParser Char st [String]
--- mul = do
---   result <- string "mul("
---   return result
+data Thing a = Do | Dont | Mul a
+  deriving (Show)
 
-parseNums :: GenParser Char st [(Int, Int)]
-parseNums = do
+skip :: GenParser Char st (Maybe (Thing (Int, Int)))
+skip = anyChar >> return Nothing
+
+parseMul :: GenParser Char st (Maybe (Thing (Int, Int)))
+parseMul = do
+  string "mul("
   a <- read <$> many1 digit
   char ','
   b <- read <$> many1 digit
   char ')'
-  return [(a, b)]
+  return $ Just (Mul (a, b))
 
-doTheThing :: GenParser Char st [(Int, Int)]
+parseDo :: GenParser Char st (Maybe (Thing (Int, Int)))
+parseDo = do
+  string "do()"
+  return $ Just Do
+
+parseDont :: GenParser Char st (Maybe (Thing (Int, Int)))
+parseDont = do
+  string "don't()"
+  return $ Just Dont
+
+doTheThing :: GenParser Char st [Thing (Int, Int)]
 doTheThing = do
-  manyTill anyChar (try (string' "mul("))
-  first <- try parseNums <|> return []
+  first <- try parseMul <|> skip
   next <- try doTheThing <|> return []
-  return (first <> next)
+  return $ maybe next (: next) first
+
+doTheThing2 :: GenParser Char st [Thing (Int, Int)]
+doTheThing2 = do
+  first <- try parseDo <|> try parseDont <|> try parseMul <|> skip
+  next <- try doTheThing2 <|> return []
+  return $ maybe next (: next) first
+
+calc2 :: [Thing (Int, Int)] -> St.State Bool [Int]
+calc2 [] = return []
+calc2 (x : xs) = do
+  case x of
+    Do -> put True >> calc2 xs
+    Dont -> put False >> calc2 xs
+    Mul (a, b) -> do
+      c <- get
+      if c then return ((a * b) : evalState (calc2 xs) c) else calc2 xs
 
 main' :: IO ()
 main' = do
-  contents <- concat . lines <$> readFile "../inputs/2024/Day3/input.txt"
+  contents <- concat . lines <$> readFile "../inputs/2024/Day3/sample.txt"
   -- print contents
   let r = fromRight [] (parse doTheThing "Day3" contents)
   -- mapM_ print r
-  print $ sum $ map (uncurry (*)) r
+  print $ sum $ map (\(Mul (a, b)) -> a * b) r
+  -- part 2
+  let r2 = fromRight [] (parse doTheThing2 "Day3" contents)
+  mapM_ print r2
+  print $ sum $ evalState (calc2 r2) True

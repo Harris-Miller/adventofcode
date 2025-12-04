7affe5dc85efb0efeed7bdb1c1e02fb4174f82da
harris-miller <harrismillerconsulting@gmail.com>
2025-12-03 21:47:31 -0700

    2025 day2 part2

--- rust/2025/day02/src/main.rs
+++ rust/2025/day02/src/main.rs
@@ -1,21 +1,22 @@
+use itertools::Itertools;
 use std::ops::RangeInclusive;
 
 fn main() {
     let input = include_str!("../../../../inputs/2025/Day2/input.txt");
 
     part_one(input);
+    part_two(input);
 }
 
-fn part_one(input: &str) {
-    let ranges = input.trim().split(",").map(|p| {
-        let (s, e) = p.split_once("-").unwrap();
-        (s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap())
-    });
-
-    let vectors = ranges.map(|(start, end)| start..=end);
-    let results = vectors.flat_map(process_one);
-    let total: u64 = results.sum();
-    println!("{:?}", total);
+fn create_ranges(input: &str) -> impl Iterator<Item = RangeInclusive<u64>> {
+    input
+        .trim()
+        .split(",")
+        .map(|p| {
+            let (s, e) = p.split_once("-").unwrap();
+            (s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap())
+        })
+        .map(|(start, end)| start..=end)
 }
 
 fn process_one(range: RangeInclusive<u64>) -> Vec<u64> {
@@ -29,4 +30,42 @@ fn process_one(range: RangeInclusive<u64>) -> Vec<u64> {
         })
         .map(|val| val.parse::<u64>().unwrap())
         .collect()
+}
+
+fn part_one(input: &str) {
+    let results = create_ranges(input).flat_map(process_one);
+    let total: u64 = results.sum();
+    println!("{:?}", total);
+}
+
+fn does_repeat(id: &String, by: usize) -> bool {
+    id.chars()
+        .chunks(by)
+        .into_iter()
+        .map(|c| c.collect::<String>())
+        .all_equal()
+}
+
+fn determine_splits(id: &String) -> Vec<usize> {
+    let len = id.len();
+    let half_len = len / 2;
+    (1..=half_len)
+        .filter(|&n| (len % n) == 0)
+        .collect::<Vec<usize>>()
+}
+
+fn process_two(range: RangeInclusive<u64>) -> Vec<u64> {
+    range
+        .filter(|val| {
+            let as_str = val.to_string();
+            let splits = determine_splits(&as_str);
+            splits.iter().any(|by| does_repeat(&as_str, *by))
+        })
+        .collect::<Vec<u64>>()
+}
+
+fn part_two(input: &str) {
+    let results = create_ranges(input).flat_map(process_two);
+    let total: u64 = results.sum();
+    println!("{:?}", total);
 }
